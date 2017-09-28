05174bd6cc247e2ef734ff54b3d2f248bbd862ea
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-09-28 03:40:21 +0000

    Added xamarin project

---
+++ Source/WeddingPhotos.MobileApps/WeddingPhotos.MobileApps/WeddingPhotos.MobileApps/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeddingPhotos.MobileApps
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
