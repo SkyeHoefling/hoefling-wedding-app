7606094c07660f9164bffff365d7ccd1cb741c17
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-10-01 18:55:41 -0400

    Added image get code to pull image file from the azure container

--- Source/WeddingPhotos.Functions/ImageGet.cs
+++ Source/WeddingPhotos.Functions/ImageGet.cs
@@ -6,14 +6,31 @@ using System.Collections.Generic;
 using WeddingPhotos.Data.Entities;
 using WeddingPhotos.Data;
 using System.Linq;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
 
 namespace WeddingPhotos.Functions
 {
     public class ImageGet
     {
-        public static IActionResult Run(HttpRequest req, TraceWriter log)
+        public static async Task<IActionResult> Run(HttpRequest req, TraceWriter log)
         {
-            return new OkObjectResult("OK");
+            var credentials = new StorageCredentials("hoeflingwedding", "HAItH1JIV7jWPLgHVq7cpcYPmZ7OXqb298HKpkxpNCcxYFIx9mCxV7VJJ4opd/+H8rsJIoc5hbH+kw+jSClPaA==");
+            var storageAccount = new CloudStorageAccount(credentials, true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("photos");
+            var block = container.GetBlockBlobReference("test.jpg");
+
+            Stream stream = await block.OpenReadAsync();
+            var ms = new MemoryStream();
+            await block.DownloadToStreamAsync(ms);
+            var response = new OkObjectResult(stream);
+            response.ContentTypes.Add(new MediaTypeHeaderValue("image/jpeg"));
+            return response;
         }
     }    
 }
