2ca3054716e47e98f5def4315f8d007053388ff7
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-10-01 22:15:19 -0400

    Updated ImageGet to take a query parameter to find the specified image.

--- Source/WeddingPhotos.Functions/ImageGet.cs
+++ Source/WeddingPhotos.Functions/ImageGet.cs
@@ -18,16 +18,21 @@ namespace WeddingPhotos.Functions
     {
         public static async Task<IActionResult> Run(HttpRequest req, TraceWriter log)
         {
+            req.Query.TryGetValue("name", out StringValues values);
+            var name = values.FirstOrDefault();
+
+            if(string.IsNullOrWhiteSpace(name)){
+                return new BadRequestResult();
+            }
+
             var credentials = new StorageCredentials("hoeflingwedding", "HAItH1JIV7jWPLgHVq7cpcYPmZ7OXqb298HKpkxpNCcxYFIx9mCxV7VJJ4opd/+H8rsJIoc5hbH+kw+jSClPaA==");
             var storageAccount = new CloudStorageAccount(credentials, true);
 
             var blobClient = storageAccount.CreateCloudBlobClient();
             var container = blobClient.GetContainerReference("photos");
-            var block = container.GetBlockBlobReference("test.jpg");
+            var block = container.GetBlockBlobReference($"{name}.jpg");
 
             Stream stream = await block.OpenReadAsync();
-            var ms = new MemoryStream();
-            await block.DownloadToStreamAsync(ms);
             var response = new OkObjectResult(stream);
             response.ContentTypes.Add(new MediaTypeHeaderValue("image/jpeg"));
             return response;
