345124af7ecf20fb7e8fb77e6dd5eb594c559e22
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-10-03 03:58:53 +0000

    removed azure functions and restructed mobile project directories

--- Source/WeddingPhotos.Data/Entities/Event.cs
+++
@@ -1,9 +0,0 @@
-namespace WeddingPhotos.Data.Entities
-{
-    public class Event
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-    }
-}
