4f7e84641a3f3cf72aef2de46a93089c59e56342
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-10-05 19:49:05 +0000

    added new resources to android project and added data to collection in ship page

--- Source/WeddingPhotos.Mobile/ViewModels/ShipViewModel.cs
+++ Source/WeddingPhotos.Mobile/ViewModels/ShipViewModel.cs
@@ -32,7 +32,20 @@ namespace WeddingPhotos.Mobile.ViewModels
         {
             var decks = new []
             {
-                new ShipDeck("Deck 3", "deck3.png", "deck3_map.png")
+                new ShipDeck("Deck 3", "deck3.png", "deck3_map.png"),
+                new ShipDeck("Deck 4", "deck4.png", "deck4_map.png"),
+                new ShipDeck("Deck 5", "deck5.png", "deck5_map.png"),
+                new ShipDeck("Deck 6", "deck6.png", "deck6_map.png"),
+                new ShipDeck("Deck 7", "deck7.png", "deck7_map.png"),
+                new ShipDeck("Deck 8", "deck8.png", "deck8_map.png"),
+                new ShipDeck("Deck 9", "deck9.png", "deck9_map.png"),
+                new ShipDeck("Deck 10", "deck10.png", "deck10_map.png"),
+                new ShipDeck("Deck 11", "deck11.png", "deck11_map.png"),
+                new ShipDeck("Deck 12", "deck12.png", "deck12_map.png"),
+                new ShipDeck("Deck 13", "deck13.png", "deck13_map.png"),
+                new ShipDeck("Deck 14", "deck14.png", "deck14_map.png"),
+                new ShipDeck("Deck 15", "deck15.png", "deck15_map.png"),
+                new ShipDeck("Deck 16", "deck16.png", "deck16_map.png"),
             };
 
             Decks = new ObservableCollection<ShipDeck>(decks);
